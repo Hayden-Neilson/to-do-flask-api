2a3d2c04a04c80a073e7999c1833658369ea1bd1
Hayden-Neilson <haydenneilson5@gmail.com>
2020-04-07 10:47:08 -0600

    todoapp

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ class Todo(db.Model):
 class TodoSchema(ma.Schema):
   class Meta:
     fields = ("id", "title", "done")
+
 todo_schema = TodoSchema()
 todos_schema = TodoSchema(many=True)
 
@@ -38,8 +39,45 @@ def get_todos():
   return jsonify(result)
 
 # POST
+@app.route("/todo", methods=["POST"])
+def add_todo():
+  title = request.json["title"]
+  done = request.json["done"]
+
+  new_todo = Todo(title, done)
+
+  db.session.add(new_todo)
+  db.session.commit()
+
+  todo = Todo.query.get(new_todo.id)
+  return todo_schema.jsonify(todo)
+
+
+
 # PUT / PATCH
+
+@app.route("/todo/<id>", methods=["PATCH"])
+def update_todo(id):
+  todo = Todo.query.get(id)
+
+  new_done = request.json["done"]
+
+  todo.done = new_done
+
+  db.session.commit()
+
+  return todo_schema.jsonify(todo)
+
+
 # DELETE
+
+@app.route("/todo/<id>", methods=["DELETE"])
+def remove_todo(id):
+  record = Todo.query.get(id)
+  db.session.delete(record)
+  db.session.commit()
+
+  return jsonify("delete that ish")
 
 if __name__ == "__main__":
   app.debug = True
